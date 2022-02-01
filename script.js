da41803dfed157a06b597ad6d8ad38374d773d53
Gabriel Porto <7gabrielporto@gmail.com>
2022-01-31 21:22:01 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const body = document.querySelector("body"),
+        sidebar = body.querySelector(".side-bar"),
+        toggle = body.querySelector(".toggle"),
+        searchBtn = body.querySelector(".search-box"),
+        modeSwitch = body.querySelector(".toggle-switch"),
+        modeText = body.querySelector(".mode-text");
+
+        toggle.addEventListener("click", () => {
+            console.log("teste")
+            sidebar.classList.toggle("close");
+        });
+
+        modeSwitch.addEventListener("click", () => {
+            body.classList.toggle("dark");
+
+            if(body.classList.contains("dark")){
+                modeText.innerText = "Light Mode"
+            } else {
+                modeText.innerText = "Dark Mode"
+            }
+        });
